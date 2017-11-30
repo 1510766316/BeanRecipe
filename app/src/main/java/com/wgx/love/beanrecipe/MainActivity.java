ec9254aad3f3ce8f6e33b360f1537f9a9e1c1ba0
guangxi.wu <guangxi.wu@imotorinfo.com>
2017-11-30 18:29:51 +0800

    add glide test

--- app/src/main/java/com/wgx/love/beanrecipe/MainActivity.java
+++ app/src/main/java/com/wgx/love/beanrecipe/MainActivity.java
@@ -1,15 +1,33 @@
 package com.wgx.love.beanrecipe;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.bumptech.glide.load.MultiTransformation;
+import com.bumptech.glide.load.resource.bitmap.FitCenter;
+import com.wgx.love.beanrecipe.glideModule.GlideApp;
+
+import static com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade;
 
 public class MainActivity extends AppCompatActivity {
 
     private final static String TAG = "MainActivity";
 
+    private ImageView imageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.image);
+        show();
+    }
+
+
+    private void show(){
+        String url = "http://img0.imgtn.bdimg.com/it/u=2069423848,1745692628&fm=27&gp=0.jpg";
+        GlideApp.with(this).asDrawable()
+                .transition(withCrossFade()).centerCrop().miniThumb().dontTransform().load(url).into(imageView);
     }
 }
---
+++ app/src/main/java/com/wgx/love/beanrecipe/glideModule/MyAppGlideExtension.java
@@ -0,0 +1,22 @@
+package com.wgx.love.beanrecipe.glideModule;
+
+import com.bumptech.glide.annotation.GlideExtension;
+import com.bumptech.glide.annotation.GlideOption;
+import com.bumptech.glide.request.RequestOptions;
+
+/**
+ * Created by imotor on 17-11-30.
+ */
+
+@GlideExtension
+public class MyAppGlideExtension {
+    // Size of mini thumb in pixels.
+    private static final int MINI_THUMB_SIZE = 400;
+
+    private MyAppGlideExtension() { } // utility class
+
+    @GlideOption
+    public static void miniThumb(RequestOptions options) {
+        options.fitCenter().override(MINI_THUMB_SIZE);
+    }
+}
